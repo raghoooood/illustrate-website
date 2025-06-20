345053386455d146e58c267de8d592d19e582337
raghoooood <raghad.nakshou@gmail.com>
2025-06-20 16:05:49 +0300

    json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import csp from 'vite-plugin-csp'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    csp({
+      policies: {
+        'default-src': [ "'self'" ],
+        'script-src': [ "'self'" ],
+        'style-src': [ "'self'", "'unsafe-inline'" ],
+        'img-src': [ "*" ]
+      }
+    })
+  ]
 })
